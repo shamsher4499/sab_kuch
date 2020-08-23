4e694e642d48be1c865fed4ff0c8cdeb7c6ebbf7
Shamsher Singh Chauhan <chauhanshamsher1234@gmail.com>
2020-08-23 14:25:04 +0530

    Added html and css file and also add images and models

--- sab_kuch/online/urls.py
+++ sab_kuch/online/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('logout/', views.logout, name='logout'),
+    path('register/', views.register, name='register'),
+    path('cart/', views.cart, name='cart'),
+    path('products/', views.products, name='products'),
+    path('customer/', views.customer, name='customer'),
 ]
--- sab_kuch/online/views.py
+++ sab_kuch/online/views.py
@@ -4,3 +4,24 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'online/index.html')
+
+def cart(request):
+    return render(request, 'online/cart.html')
+
+def login(request):
+    return render(request, 'online/login.html')
+
+def logout(request):
+    return render(request, 'online/logout.html')
+
+def register(request):
+    return render(request, 'online/register.html')
+
+def checkout(request):
+    return render(request, 'online/checkout.html')
+
+def products(request):
+    return render(request, 'online/products.html')
+
+def customer(request):
+    return render(request, 'online/customer.html')
